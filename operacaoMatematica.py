7fb1d52a3ea26116f3e3da5d81c8be58f50f2744
wallaceIniciante <wallatag@gmail.com>
2025-03-14 02:04:39 +0000

    adicionando arquivo operacaoMatematica

---
+++ operacaoMatematica.py
@@ -0,0 +1,18 @@
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+operacao = input("Qual operacao? ex(+,-,*,/) ")
+
+if operacao == "+":
+    print(num1 + num2)
+
+elif operacao == "-":
+    print(num1 - num2)
+
+elif operacao == "*":
+    print(num1 * num2)
+
+elif operacao == "/":
+    print(num1 / num2)
+
+else:
+    print("Operacao Inválida!")
