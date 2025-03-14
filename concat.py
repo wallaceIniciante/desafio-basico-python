62a7f4f154a8b6324dae2f6f439cbec38f681312
wallaceIniciante <wallatag@gmail.com>
2025-03-14 01:56:53 +0000

    editando arquivo repetir

---
+++ concat.py
@@ -0,0 +1,6 @@
+
+nome = input("Digite seu nome: ")
+sobrenome = input("Sobrenome: ")
+nomeCompleto = nome + " " + sobrenome
+
+print("Nome: " + nomeCompleto);
---
+++ repetir.py
@@ -0,0 +1,5 @@
+string = input("Digite uma String: ")
+qtd = input("Digite quantas vezes vai repetir: ")
+resultado = (string + " ") * qtd
+
+print(resultado)
